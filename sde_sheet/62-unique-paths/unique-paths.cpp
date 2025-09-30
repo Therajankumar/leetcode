8862cdfe27ec0a6e19186e0398c73508e88fd3e5
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-09-30 19:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.68%) - LeetSync

--- sde_sheet/62-unique-paths/unique-paths.cpp
+++ sde_sheet/62-unique-paths/unique-paths.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int dp[102][102];
-    int countpaths(int i, int j, int m, int n) {
-        if ((i == n - 1) && (j == m - 1))
-            return 1;
-        else if (i >= n || j >= m)
-            return 0;
-        else if (dp[i][j] != -1)
-            return dp[i][j];
-        else
-            return dp[i][j] =
-                       countpaths(i + 1, j, m, n) + countpaths(i, j + 1, m, n);
-    }
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return countpaths(0, 0, m, n);
+       int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++){
+                res = res * (N - r + i) / i;
+           
+       }
+     return (int)res;
     }
 };
