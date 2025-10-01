a9678d56dc0870303ddfc28955fe53445abd9cda
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-10-02 00:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.71%) - LeetSync

--- sde_sheet/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ sde_sheet/21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,30 +10,27 @@
  */
 class Solution {
 public:
-ListNode* arrayToList(vector<int>& arr) {
-    if (arr.empty()) return NULL;
-    ListNode* head = new ListNode(arr[0]);
-    ListNode* curr = head;
-    for (int i = 1; i < arr.size(); i++) {
-        curr->next = new ListNode(arr[i]);
-        curr = curr->next;
-    }
-    return head;
-}
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> arr;
-        ListNode* temp1 =list1;
-        ListNode* temp2 =list2;
+       ListNode* t1 =list1;
+       ListNode* t2 =list2;
+       
+       ListNode* dnode = new ListNode(-1);
+       ListNode* temp = dnode;
 
-        while(temp1 != NULL){
-            arr.push_back(temp1->val);
-            temp1 =temp1->next;
+       while(t1 != NULL && t2 != NULL){
+        if((t1->val) < (t2->val)){
+            temp->next =t1;
+            temp =t1;
+            t1=t1->next;
         }
-        while(temp2 != NULL){
-            arr.push_back(temp2->val);
-            temp2 =temp2->next;
+        else{
+            temp->next=t2;
+            temp=t2;
+            t2=t2->next;
         }
-        sort(arr.begin(),arr.end());
-        return arrayToList(arr);
+       }
+       if(t1) temp->next =t1;
+       else temp->next =t2;
+       return dnode->next;
     }
 };
