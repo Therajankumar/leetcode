f1ad81805b3b92d9fff2a6537959c4efb88c59ac
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-10-10 21:10:04 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.25%) - LeetSync

---
+++ sde_sheet/63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   int uniquepathutil(int m , int n ,vector<vector<int>>& obstacleGrid,vector<vector<int>> &dp){
+    if(m>=0 && n>=0 && obstacleGrid[m][n] ==1) return 0;
+    if(m==0 && n==0) return 1;
+    if(m <0 || n<0) return 0;
+
+    if(dp[m][n] !=-1) return dp[m][n];
+
+   int up =uniquepathutil(m-1 ,n ,obstacleGrid ,dp);
+   int left =uniquepathutil(m ,n-1,obstacleGrid,dp);
+
+   return dp[m][n] =up + left;
+   }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m =obstacleGrid.size();
+        int n =obstacleGrid[0].size();
+         vector<vector<int>>dp(m, vector<int>(n, -1));
+         return  uniquepathutil(m-1,n-1,obstacleGrid ,dp);
+    }
+};
