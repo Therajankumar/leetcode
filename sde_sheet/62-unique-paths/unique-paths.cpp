0eb8a7561c2ca084f051dae24d8cb7761e4c9ca5
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-10-10 20:57:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (10.53%) - LeetSync

--- sde_sheet/62-unique-paths/unique-paths.cpp
+++ sde_sheet/62-unique-paths/unique-paths.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+   int uniquepathutil(int m , int n ,vector<vector<int>> &dp){
+    if(m==0 && n==0) return 1;
+    if(m <0 || n<0) return 0;
+
+    if(dp[m][n] !=-1) return dp[m][n];
+
+   int up =uniquepathutil(m-1 ,n ,dp);
+   int left =uniquepathutil(m ,n-1,dp);
+
+   return dp[m][n] =up + left;
+   }
     int uniquePaths(int m, int n) {
-       int N = n + m - 2;
-            int r = m - 1; 
-            double res = 1;
-            
-            for (int i = 1; i <= r; i++){
-                res = res * (N - r + i) / i;
-           
-       }
-     return (int)res;
+       vector<vector<int>>dp(m, vector<int>(n, -1));
+
+       return uniquepathutil(m-1 ,n-1 ,dp);
     }
 };
