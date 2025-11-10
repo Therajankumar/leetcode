7e4eba3ffff22cd9cf8e12ff856c524ded0c5866
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-11-10 14:17:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.46%) - LeetSync

--- sde_sheet/70-climbing-stairs/climbing-stairs.cpp
+++ sde_sheet/70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-     int climbStairsutility(int n,vector<int>&dp) {
-       if(n==0 || n==1) return 1 ;
-      if(dp[n] !=-1) return dp[n];
-       for(int i =2 ;i <=n;i++){
-        return  dp[n] = climbStairsutility(n-1,dp)+climbStairsutility(n-2,dp);
-       }
-       return 0;
+    int f(int ind ,vector<int>&dp ){
+        if(ind ==0 || ind==1) return 1;
+        if(dp[ind] !=-1) return dp[ind];
+        int left =f(ind-1,dp);
+        int right=f(ind-2,dp);
+        return dp[ind]=left + right;
     }
     int climbStairs(int n) {
-        vector<int>dp(n+1 ,-1);
-        return climbStairsutility(n,dp);
+       vector<int>dp(n+1,-1);
+       return f(n ,dp);
     }
 };
