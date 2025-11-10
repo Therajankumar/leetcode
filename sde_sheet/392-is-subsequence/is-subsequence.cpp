88fae86f434b639289603be57028c0c45f57a660
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-11-10 15:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (5.18%) - LeetSync

--- sde_sheet/392-is-subsequence/is-subsequence.cpp
+++ sde_sheet/392-is-subsequence/is-subsequence.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    bool helper(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
+        // base cases
+        if (i == s.size()) return true;     
+        if (j == t.size()) return false;    // t ended before s matched
+        
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] == t[j])
+            return dp[i][j] = helper(i + 1, j + 1, s, t, dp);
+        else
+            return dp[i][j] = helper(i, j + 1, s, t, dp);
+    }
+
     bool isSubsequence(string s, string t) {
-        int i = 0, j = 0;
-        while (i < s.size() && j < t.size()) {
-            if (s[i] == t[j]) i++;
-            j++;
-        }
-        return i == s.size();
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, -1));
+        return helper(0, 0, s, t, dp);
     }
 };
