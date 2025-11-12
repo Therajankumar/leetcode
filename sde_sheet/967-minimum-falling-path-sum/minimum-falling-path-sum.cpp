630f17c39dabe171b2c3981fba6ccfc4601e1c03
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-11-12 14:36:22 +0530

    Time: 3 ms (33.21%) | Memory: 15 MB (60.56%) - LeetSync

--- sde_sheet/967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ sde_sheet/967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
+    int utility(int i, int j, vector<vector<int>>& matrix,
+        vector<vector<int>>& dp) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        // base case
+       if (j < 0 || j >= m) return 1e9;
+       if(i==n-1) return matrix[i][j];
+       
+        if (dp[i][j] != -101)
+            return dp[i][j];
+        int  down = matrix[i][j] + utility(i + 1, j, matrix, dp);
+        int  diagonalR = matrix[i][j] + utility(i + 1, j + 1, matrix, dp);
+        int diagonalL = matrix[i][j] + utility(i + 1, j - 1, matrix, dp);
+
+        return dp[i][j] = min(down, min(diagonalL, diagonalR));
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<vector<int>> dp(n, vector<int>(m, -101));
+       int ans = INT_MAX;
 
-        // base case: first row same as matrix
+        // start from top row, try every column
         for (int j = 0; j < m; j++)
-            dp[0][j] = matrix[0][j];
-
-        // fill from 2nd row to last
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int up = matrix[i][j] + dp[i - 1][j];
-
-                int leftDiag = matrix[i][j];
-                if (j - 1 >= 0)
-                    leftDiag += dp[i - 1][j - 1];
-                else
-                    leftDiag = 1e9;
-
-                int rightDiag = matrix[i][j];
-                if (j + 1 < m)
-                    rightDiag += dp[i - 1][j + 1];
-                else
-                    rightDiag = 1e9;
-
-                dp[i][j] = min({up, leftDiag, rightDiag});
-            }
-        }
-
-        // answer = minimum of last row
-        int ans = INT_MAX;
-        for (int j = 0; j < m; j++)
-            ans = min(ans, dp[n - 1][j]);
+            ans = min(ans, utility(0, j, matrix, dp));
 
         return ans;
     }
