a4b43f04b6e9740731cbee17556a3b96325b35d5
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-11-27 23:55:42 +0530

    Time: 27 ms (83.01%) | Memory: 54.3 MB (60.05%) - LeetSync

---
+++ sde_sheet/1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+         sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int mod = 1e9 + 7;
+
+        vector<int> pow2(n);
+        pow2[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            pow2[i] = (pow2[i-1] * 2) % mod;
+        }
+
+        int l = 0, r = n - 1;
+        long long ans = 0;
+
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                ans = (ans + pow2[r - l]) % mod;
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return ans;
+    }
+};
