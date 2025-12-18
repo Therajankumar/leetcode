a715ab6ccb8bf8a57a50b7445b7c9c318da9a907
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2025-12-19 00:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (82.11%) - LeetSync

---
+++ sde_sheet/4158-absolute-difference-between-maximum-and-minimum-k-elements/absolute-difference-between-maximum-and-minimum-k-elements.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int absDifference(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        return accumulate(nums.end()-k ,nums.end(),0) - accumulate(nums.begin(),nums.begin()+k,0);
+    }
+};
