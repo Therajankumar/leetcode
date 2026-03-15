95c4f45a61906131e2c92052e9603f83398cb6b8
RAJAN KUMAR <kumarrajanthakur2005@gmail.com>
2026-03-15 16:18:55 +0530

    Time: 3 ms (69.84%) | Memory: 16.3 MB (16.52%) - LeetSync

--- sde_sheet/22-generate-parentheses/generate-parentheses.cpp
+++ sde_sheet/22-generate-parentheses/generate-parentheses.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     void helper(int n ,int open , int close , string str ,vector<string> &ans){
-        if(open ==0 && close == 0){
+        if(open ==n && close == n){
             ans.push_back(str);
             return;
         }
-        if(open !=0){
+        if(open !=n){
             string op1 =str;
             op1.push_back('(');
-           helper(n ,open -1 ,close ,op1,ans);
+           helper(n ,open +1 ,close ,op1,ans);
         }
         
-        if(open < close){
+        if(open > close){
             string op2 =str;
             op2.push_back(')');
-            helper(n,open ,close-1 ,op2,ans);
+            helper(n,open ,close+1 ,op2,ans);
         }
     }
     vector<string> generateParenthesis(int n) {
      vector<string>ans;
      string str="";
-     helper(n,n,n,str,ans);
+     helper(n,0,0,str,ans);
      return ans;
     }
 };
